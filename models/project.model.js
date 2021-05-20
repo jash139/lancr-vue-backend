520354db723c62c2559131c1f0fd969f35aef1e5
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-20 13:26:52 +0530

    Change code formatting

--- models/project.model.js
+++ models/project.model.js
@@ -4,19 +4,24 @@ const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
     uid: { type: String, required: true },
-
     title: String,
     description: String,
-
     status: String,
-
     requirements: [{ type: String }],
-    offeredAmount: { currencyType: String, start: Number, end: Number },
-
-    timePeriod: { start: String, end: String },
-
-    location: { country: String, state: String, city: String },
-
+    offeredAmount: {
+        currencyType: String,
+        start: Number,
+        end: Number
+    },
+    timePeriod: {
+        start: String,
+        end: String
+    },
+    location: {
+        country: String,
+        state: String,
+        city: String
+    },
     applicants: [{ type: String }]
 },
     { timestamps: true }
